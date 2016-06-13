9a2a2c69c6702b38d9d46c321bcaa6be32111218
Benjamin Heflin and Kyle Helyar <kyle@>
2016-06-13 09:13:54 -0700

    add business logic place constructor and 3 object places

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function Place(location, landmarks, time, notes) {
+  this.placeLocation = location;
+  this.placeLandmarks = landmarks;
+  this.placeTime = time;
+  this.placeNotes = notes;
+}
+
+var england = new Place ('England', ['Big Ben', 'Buckingham Palace', 'London Eye'], 'April through September', 'Top o the morning, Governor!');
+
+var japan = new Place ('Japan', ['Tokyo Tower', 'Auto Salon', 'Asakusa Temple'], 'April 29 to May 5', 'Osu!');
+
+var bahamas = new Place ('Bahamas', ['Nassau', 'Fort Fincastle', 'Parliamant Square'], 'June through August', 'You put the lime in the coconut');
