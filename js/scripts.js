759733a380a33c10cdcd80653a28b476d877a0d7
Benjamin Heflin and Kyle Helyar <benjamin@>
2016-06-13 10:49:05 -0700

    fix html layout

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,8 @@ function Place(location, landmarks, time, notes) {
   this.placeNotes = notes;
 }
 
-var england = new Place ('England', ['Big Ben', 'Buckingham Palace', 'London Eye'], 'April through September', 'Top o the morning, Governor!');
-
-var japan = new Place ('Japan', ['Tokyo Tower', 'Auto Salon', 'Asakusa Temple'], 'April 29 to May 5', 'Osu!');
-
-var bahamas = new Place ('Bahamas', ['Nassau', 'Fort Fincastle', 'Parliamant Square'], 'June through August', 'You put the lime in the coconut');
+// var england = new Place ('England', ['Big Ben', 'Buckingham Palace', 'London Eye'], 'April through September', 'Top o the morning, Governor!');
+//
+// var japan = new Place ('Japan', ['Tokyo Tower', 'Auto Salon', 'Asakusa Temple'], 'April 29 to May 5', 'Osu!');
+//
+// var bahamas = new Place ('Bahamas', ['Nassau', 'Fort Fincastle', 'Parliamant Square'], 'June through August', 'You put the lime in the coconut');
